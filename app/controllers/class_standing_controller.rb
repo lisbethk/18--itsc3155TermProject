8f5d693548b6b389cd44d1053ef82fb3e1e4b104
Ksenia Andreeva <kseniabeth@gmail.com>
2019-03-14 13:03:24 -0400

    Pass User standing to Welcome controller

--- app/controllers/class_standing_controller.rb
+++ app/controllers/class_standing_controller.rb
@@ -2,4 +2,21 @@ class ClassStandingController < ApplicationController
   def index
 
   end
+
+  def create
+    @User = User.new
+    if params[:freshman].present?
+      @User.standing = "freshman"
+    elsif params[:sophomore].present?
+      @User.standing = "sophomore"
+    elsif params[:junior].present?
+      @User.standing = "junior"
+    elsif params[:senior].present?
+      @User.standing = "senior"
+    elsif params[:nontraditional].present?
+      @User.standing = "nontraditional"
+    end
+    @User.save
+    redirect_to welcome_index_path(:user_id => @User.id)
+  end
 end

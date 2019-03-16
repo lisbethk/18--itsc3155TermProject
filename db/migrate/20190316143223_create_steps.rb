7210f6c13ed30bad062e8cecb086617e70ed43c4
Jane K King <kseniabeth@gmail.com>
2019-03-16 12:19:05 -0400

    Fix Environment data not found in the schema

--- db/migrate/20190316143223_create_steps.rb
+++ db/migrate/20190316143223_create_steps.rb
@@ -1,9 +1,8 @@
 class CreateSteps < ActiveRecord::Migration[5.2]
   def change
-    drop_table :steps
     create_table :steps do |t|
       t.string :content
-
+      t.string :extra
       t.timestamps
     end
   end
--- db/migrate/20190316144331_remove_steps_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveStepsFromUsers < ActiveRecord::Migration[5.2]
-  def self.up
-    remove_column :users, :steps
-  end
-end

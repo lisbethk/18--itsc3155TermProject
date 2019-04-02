a223989a086b25f5853035da1f2f958f7ab102e9
Ksenia Andreeva <kseniabeth@gmail.com>
2019-04-02 12:52:26 -0400

    Try to get ssl cert

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   resources :timeline, only: [:index]
   
   root 'class_standing#index'
+  get '/.well-known/acme-challenge/:id' => 'class_standing#letsencrypt', constraints: { :id => /[0-z\.]+/ }
 end

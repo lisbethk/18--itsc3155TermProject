10115999bf25f11076a8f303cc55e990a1ba8676
Ksenia Andreeva <kseniabeth@gmail.com>
2019-04-02 12:52:26 -0400

    Try to get ssl cert

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,22 @@
 
 ActiveRecord::Schema.define(version: 2019_03_16_150324) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "steps", force: :cascade do |t|
     t.string "content"
     t.string "extra"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id"
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_steps_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.text "Description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -29,4 +38,5 @@ ActiveRecord::Schema.define(version: 2019_03_16_150324) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "steps", "users"
 end

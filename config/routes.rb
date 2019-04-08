dcc98933fb68ca7100c580951dfc56eaa6fbbd61
Ksenia Andreeva <kseniabeth@gmail.com>
2019-04-08 12:54:05 -0400

    Add logout button

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   get 'auth/auth0/callback' => 'auth0#callback'
   get 'auth/failure' => 'auth0#failure'
+  get 'auth/logout' => 'logout#logout'
 
   root 'class_standing#index'
 end

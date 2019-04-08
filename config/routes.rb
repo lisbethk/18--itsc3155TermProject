70565208094555f8f391768b3ea776be7eb494d2
Ksenia Andreeva <kseniabeth@gmail.com>
2019-04-08 11:15:27 -0400

    Remove letsencrypt route

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   resources :welcome
   resources :class_standing, only: [:index, :create]
   resources :timeline, only: [:index]
-  
-  get '/.well-known/acme-challenge/:id' => 'class_standing#letsencrypt', constraints: { :id => /[0-z\.]+/ }
 
   get 'auth/auth0/callback' => 'auth0#callback'
   get 'auth/failure' => 'auth0#failure'

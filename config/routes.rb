fc5d28cd14156d0643fcbbf17930a7f94a22ddc0
Ksenia Andreeva <kseniabeth@gmail.com>
2019-04-08 11:18:56 -0400

    Destroy (temp) dashboard controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,5 @@ Rails.application.routes.draw do
   get 'auth/auth0/callback' => 'auth0#callback'
   get 'auth/failure' => 'auth0#failure'
 
-  get 'dashboard' => 'dashboard#show'
-
   root 'class_standing#index'
 end

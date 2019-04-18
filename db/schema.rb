a480a5bc1639fa3fac1288e82f5703f4437e16ad
Ksenia Andreeva <kseniabeth@gmail.com>
2019-04-18 16:56:44 -0400

    Add email to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,32 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_16_150324) do
+ActiveRecord::Schema.define(version: 2019_04_18_194852) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'steps', force: :cascade do |t|
-    t.string 'content'
-    t.string 'extra'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id'
-    t.index ['user_id'], name: 'index_steps_on_user_id'
+  create_table "steps", force: :cascade do |t|
+    t.string "content"
+    t.string "extra"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_steps_on_user_id"
   end
 
-  create_table 'tasks', force: :cascade do |t|
-    t.text 'Description'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "standing"
+    t.string "experience"
+    t.string "goal"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'standing'
-    t.string 'experience'
-    t.string 'goal'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-  end
-
-  add_foreign_key 'steps', 'users'
+  add_foreign_key "steps", "users"
 end
